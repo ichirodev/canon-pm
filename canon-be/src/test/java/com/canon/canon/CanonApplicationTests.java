84b60fd4d7a966ede5d297f3f81cbae0cb1d227b
ichirodev <adanschiffer@hotmail.com>
2022-07-22 16:27:27 -0500

    Initialized Spring Project

---
+++ canon-be/src/test/java/com/canon/canon/CanonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canon.canon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
