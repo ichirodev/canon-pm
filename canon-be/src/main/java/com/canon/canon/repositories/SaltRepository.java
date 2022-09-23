50d490b03f391dd354e3f8550bc87a4488b20a68
Adan Schiffer <adanschiffer@hotmail.com>
2022-09-23 23:56:32 +0100

    Removed old back-end project

--- canon-be/src/main/java/com/canon/canon/repositories/SaltRepository.java
+++
@@ -1,8 +0,0 @@
-package com.canon.canon.repositories;
-
-import com.canon.canon.models.Salt;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SaltRepository extends MongoRepository<Salt, ObjectId> {
-}
